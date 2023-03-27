0ccedcf1210982de44530d09e155f93f9cc783d7
My Secont Github Repo <hmsadikurrahman120@gmail.com>
2023-03-27 10:07:34 +0600

    ad to header part

--- src/component/Header/Header.jsx
+++ src/component/Header/Header.jsx
@@ -1,9 +1,17 @@
 import React from 'react';
+import './Header.css'
+import Logo from "../../images/Logo.svg";
 
 const Header = () => {
     return (
-        <div>
-            <h1>Hallo form ema jon resources</h1>
+        <div className='header'>
+            <img src={Logo} alt="" />
+            <nav>
+                <a href="/order">Order</a>
+                <a href="">Order Review</a>
+                <a href="">Mange inventory</a>
+                <a href="">Login</a>
+            </nav>
         </div>
     );
 };
