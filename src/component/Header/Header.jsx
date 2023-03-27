5aa96572c12e7c24ba784dcc9fb781b51262c8d7
My Secont Github Repo <hmsadikurrahman120@gmail.com>
2023-03-27 09:23:06 +0600

    load to emajon web file

---
+++ src/component/Header/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div>
+            <h1>Hallo form ema jon resources</h1>
+        </div>
+    );
+};
+
+export default Header;
