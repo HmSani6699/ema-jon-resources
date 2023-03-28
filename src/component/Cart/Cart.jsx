b9ccea3ef5819143e5610332f13d1ab57ef4d4a2
My Secont Github Repo <hmsadikurrahman120@gmail.com>
2023-03-28 10:44:24 +0600

    ad to cart component and calclutaion

--- src/component/Cart/Cart.jsx
+++ src/component/Cart/Cart.jsx
@@ -2,18 +2,23 @@ import React from 'react';
 import './Cart.css';
 
 const Cart = ({ cart }) => {
+    console.log(cart)
 
-    let total = 0;
+    let totalPrice = 0;
+    let totalShipping = 0;
     for (const product of cart) {
-        total = total + product.price;
+        totalPrice = totalPrice + product.price;
+        totalShipping = totalShipping + product.shipping;
     }
+    let text = totalPrice * 7 / 100;
+
     return (
         <div className='cart'>
             <h2 className='cart-title'>Order summary: {cart.length}</h2>
-            <p>Total price: {total}</p>
-            <p>Total shipping charge: { }</p>
-            <p>Tex: ${ }</p>
-            <h6>Grand total: ${ }</h6>
+            <p>Total price: ${totalPrice}</p>
+            <p>Total shipping charge: ${totalShipping}</p>
+            <p>Tex: ${text.toFixed(2)}</p>
+            <h6 className='gand-total'>Grand total: ${ }</h6>
         </div>
     );
 };
